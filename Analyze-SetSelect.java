ce670a312c5e6dd87983c5a84ed31c376dbdb0f7
Imran <imranmahammad230@gmail.com>
2026-01-21 12:21:39 +0530

    program1

---
+++ Analyze-SetSelect.java
@@ -0,0 +1,20 @@
+package div1;
+import java.util.Map;
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+public class Collect_6 {
+    public static void main(String[] args) {
+
+        Set<String> users = new LinkedHashSet<>();
+
+        users.add("Imran");
+        users.add("Divya");
+        users.add("Anil");
+
+        System.out.println(users);
+        
+        }
+    }
+
+
