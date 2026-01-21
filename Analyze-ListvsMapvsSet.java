30f6ffe03a152025fa660602625c110ed1303835
Imran <imranmahammad230@gmail.com>
2026-01-21 12:30:57 +0530

    program1

---
+++ Analyze-ListvsMapvsSet.java
@@ -0,0 +1,19 @@
+package div1;
+import java.util.*;
+
+public class Collect_6 {
+    public static void main(String[] args) {
+
+        List<String> l = new ArrayList<>();
+
+        l.add("S01(Product_id)");
+        l.add("SINDI(product_name)");
+        l.add("20(Qunatity)");
+
+        System.out.println(l);
+        
+        }
+    }
+
+
+//comments:List allow duplicate names for product id and names and quantity
